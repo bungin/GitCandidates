c2f60de05e447e46c13856853e380207608cb463
bungin <nimai.band@gmail.com>
2024-09-22 02:03:26 -0500

    vite.config and interface

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,14 @@
-// TODO: Create an interface for the Candidate objects returned by the API
+// DO: Create an interface for the Candidate objects returned by the API
+interface Candidate {
+    id: number;
+    login: string | null;
+    name: string | null;
+    html_url: string | null;
+    email: string | null;
+    avatar_url: string | null;
+    company: string | null;
+    bio: string | null;
+    location: string | null;
+}
+
+export default Candidate;
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+//https://community.render.com/t/no-open-http-ports-detected/14511/6 source for lines 7-10
 // https://vitejs.dev/config/
 export default defineConfig({
-  envDir: './env',
+  envDir: './environment',
   plugins: [react()],
+  server: {
+  host: "0.0.0.0",
+  port: 8000,
+  }
 });
